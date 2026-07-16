1bb1411a277e1492e51c0923509bbf59dac40f5f
Faustas <faustas.alekna@gmail.com>
2026-07-16 09:24:30 +0300

    Audit logs

---
+++ src/main/java/lv/acnbootcamp/fixmycity/dto/audit/AuditLogResponse.java
@@ -0,0 +1,22 @@
+package lv.acnbootcamp.fixmycity.dto.audit;
+
+import lombok.*;
+import lv.acnbootcamp.fixmycity.entity.AuditAction;
+import lv.acnbootcamp.fixmycity.entity.AuditEntityType;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuditLogResponse {
+    private Long auditLogId;
+    private AuditEntityType entityType;
+    private Long entityId;
+    private AuditAction action;
+    private String performedByEmail;
+    private String details;
+    private LocalDateTime timestamp;
+}
